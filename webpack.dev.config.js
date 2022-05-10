9b769b5e505dd7e14a60db9fb765cdefb9edb31a
fabiolafrancisco28@gmail.com <fabiolafrancisco163@gmail.com>
2022-05-10 17:42:19 -0500

    S31 -agregando materialize

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,56 +1,75 @@
 
-// importando dependencias path
-//dependencia del core de Node
+// Importando dependencia path
+// dependencia del core de Node
 const path = require("path");
+// Plugins para Webpack
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// Agregando soporte para ESLINT
+const EslintWebpackPlugin = require('eslint-webpack-plugin'); 
 
-module.exports ={
-    //1. Especificar el archivo de  entrada
-    entry:'./client/index.js',
-    //2. Especificar la salida del archivo
-    output:{
-        //2.1 Ruta bsoluta de la salida
-     path: path.resolve(__dirname,'public'),
-     //2.2 Nombre del archivo de salida
-     filename:path.join('javascripts','bundle.js'),
-     publicPath: '/',
-    },
-    //3 configurando el sercvidor de desarrollo
-    devServer:{
-        //3.1  folder de archivos estaticos
-        static: path.join(__dirname, 'public'),
-        // 3.2 Puerto del servidor desarrollo
-        port:8080,
-        //
-        host: 'localhost'
-    },
-    //4.0 Modulos
-    module: {
-        rules:[
-            //3.1 Regla de babel
-            {
-        test:/\.js$/,
+module.exports = {
+  // 1. Especificar el archivo de entrada
+  entry: "./client/index.js",
+  // 2. Especificar el archivo de salida
+  output: {
+    // 2.1 Ruta ansoluta de la salida
+    path: path.resolve(__dirname, "public"),
+    // 2.2 Nombre del archivo de salida
+    filename: path.join("javascripts", "bundle.js"),
+    // 2.3 path publico
+    publicPath: "/",
+  },
+  // 3. Configurando el servidor de desarrollo
+  devServer: {
+    // 3.1 Folder de archivos estaticos
+    static: path.join(__dirname, "public"),
+    // 3.2 Puerto del servidor de desarrollo
+    // de WP (Webpack)
+    port: 8080,
+    // 3.3 Definiendo host
+    host: "localhost",
+  },
+  // 4.0 Modulos
+  module: {
+    rules: [
+      // 4.1 Regla para Babel
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
-        use:[
-            //3.1.1 primer stage
-            {
-              loader: 'babel-loader',
-              options:{
-                  presets:[
-                      [
-                      '@babel/preset-env',{
-                          module: false,
-                          useBuiltIns: 'usage',
-                          targets:'> 0.25%, not dead',
-                          corejs:3
-                      }
-                  ]
-                ]
-              }            
-            }
-        ]
-
-        }   
-    ]
-    }
-
-}
+        use: [
+          // 4.1.1 Primer stage
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    modules: false,
+                    useBuiltIns: "usage",
+                    targets: {
+                      "chrome":"80"
+                    },
+                    corejs: 3,
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      // 3.2 Reglas para Css
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  // 4. Plugins
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: path.join("stylesheets", "styles.css"),
+    }),
+    new EslintWebpackPlugin()
+  ],
+};
