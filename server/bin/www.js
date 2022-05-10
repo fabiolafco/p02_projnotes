3bb11fb5da635741d73948ed2ca902af698f416d
fabiolafrancisco28@gmail.com <fabiolafrancisco163@gmail.com>
2022-05-10 17:58:35 -0500

    Incorporando winston

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,95 +4,92 @@
  * Module dependencies.
  */
 
-import app from '@s/app';
-
-//var debug = require('debug')('p022-projnotes:server');
-// importamos primero el debug y el la segunda parte se iso el const
-
-import Debug from 'debug'
-import http  from 'http'
-// creando instancia de debugger
-const debug = Debug ('p022-projnotes:server');
-
-/**
- * Get port from environment and store in Express.
- */
-
-
-const port = normalizePort(process.env.PORT || '3000');
-
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app);
-//pr q server es una instancia que nunca va a cambiar
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error (`${bind}  requieres elevated privileges` );
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(`${bind} is already is use` );
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? `pipe  ${addr}` : ` port $
-    {addr.port}`;
-debug(`Listening on  ${bind} `);
-console.log(`servidor escuchando  ${app.get("port")} `);
-}
+ import app from '@s/app';
+ import Debug from 'debug';
+ import http from 'http';
+ 
+ // Importando nuestro logger
+ import winston from '../config/winston';
+ 
+ // Creando instancia del debugger
+ const debug = Debug('p022_projnotes:server');
+ 
+ /**
+  * Get port from environment and store in Express.
+  */
+ 
+ const port = normalizePort(process.env.PORT || '3000');
+ // app es una instnacia de ExpressJs[ ] [ NODE ]
+ app.set('port', port);
+ 
+ /**
+  * Create HTTP server.
+  */
+ 
+ const server = http.createServer(app); // (req, res, next, err)=> {}
+ 
+ /**
+  * Listen on provided port, on all network interfaces.
+  */
+ 
+ server.listen(port); // Pone al server a escuchar
+ // Se registran eventos
+ server.on('error', onError); // En caso de error
+ server.on('listening', onListening); // Cuando esta escuchando
+ 
+ /**
+  * Normalize a port into a number, string, or false.
+  */
+ 
+ function normalizePort(val) {
+   const port = parseInt(val, 10);
+ 
+   if (isNaN(port)) {
+     // named pipe
+     return val;
+   }
+ 
+   if (port >= 0) {
+     // port number
+     return port;
+   }
+ 
+   return false;
+ }
+ 
+ /**
+  * Event listener for HTTP server "error" event.
+  */
+ 
+ function onError(error) {
+   if (error.syscall !== 'listen') {
+     throw error;
+   }
+ 
+   const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+ 
+   // handle specific listen errors with friendly messages
+   switch (error.code) {
+     case 'EACCES':
+       winston.error(`Port: ${bind} requires elevated privileges`);
+       process.exit(1);
+       break;
+     case 'EADDRINUSE':
+       winston.error(`Port: ${bind} is already in use`);
+       process.exit(1);
+       break;
+     default:
+       throw error;
+   }
+ }
+ 
+ /**
+  * Event listener for HTTP server "listening" event.
+  */
+ 
+ function onListening() {
+   const addr = server.address();
+   const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+   debug(`Listening on ${bind}`);
+   winston.info(`Servidor escuchando ...en ${app.get('port')}`);
+ }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  // render manda a renderizar (generar y entregar)
+  // la vista al cliente
+  // Calculando
+  const emojieDataset = [
+    '💻',
+    '👨‍💻',
+    '🎈',
+    '🎄',
+    '🦺',
+    '🎢',
+    '🎁',
+    '🚆',
+    '🌍',
+    '♥',
+  ];
+  const emojie =
+    emojieDataset[Math.floor(Math.random() * emojieDataset.length)];
+  res.render(
+    'index',
+    // Este es el View-Model
+    {
+      title: 'Projnotes',
+      author: 'Ivan Rivalcoba',
+      emojie,
+    }
+  );
 });
 
 module.exports = router;
